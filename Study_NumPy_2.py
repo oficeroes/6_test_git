5eaef37360a013a9ef8757b7ec1ecdf5f2656922
oficeroes <s13257@outlook.com>
2026-06-01 23:07:50 +0800

    numpy

---
+++ Study_NumPy_2.py
@@ -0,0 +1,6 @@
+import numpy as np
+# from numpy.ma.core import arange
+
+arr = np.arange(0,25).reshape(5,5)
+print(arr)
+print(type(arr.shape))
---
+++ study_numpy.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+a =np.arange(6)
+b = np.linspace(1, 10, 4)
+print(b)
