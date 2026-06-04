eef484e3a2198f7a0f120ab70e9cda45feffa3ad
oficeroes <s13257@outlook.com>
2026-06-04 19:19:14 +0800

    numpy

--- class_test_6.py
+++ class_test_6.py
@@ -1,9 +1,30 @@
-class Box():
-	def __init__(self,length1,width1,height1):
-		self.length1=length1
-		self.width1=width1
-		self.height1=height1
-	def volume(self):
-		return self.length1 * self.width1 * self.height1
-my_box=Box(10,20,30)
-print("长方体体积是%.2f"%my_box.volume())
+class Gun():
+	def __init__(self,model):
+		self.model = model
+		self.bullet_count = 0
+
+	def add_bullet(self,count):
+		self.bullet_count += count
+
+	def shoot(self):
+		if self.bullet_count <= 0:
+			print('%s没有子弹' %(self.model))
+		else:
+			self.bullet_count -= 1
+			print('%s的子弹数量为%s' %(self.model,self.bullet_count))
+class Soldier():
+	def __init__(self,name):
+		self.name =name
+		self.gun = None
+	def fire(self):
+		if self.gun == None:
+			print('%s没有枪' %(self.name))
+		else:
+			self.gun.add_bullet(5)
+			self.gun.shoot()
+ak47 = Gun('AK47')
+ak47.add_bullet(10)
+ak47.shoot()
+ruien = Soldier('RE')
+ruien.gun = ak47
+ruien.fire()
